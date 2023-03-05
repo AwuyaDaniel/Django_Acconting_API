eff1ad80e2756733caac8fcf290e4489eb360eea
Daniel <47531721+AwuyaDaniel@users.noreply.github.com>
2023-03-05 19:56:34 +0100

    develop branch

--- django_rest_api_project/settings.py
+++ django_rest_api_project/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'Accounting'
+    'Accounting',
+    'Company',
 ]
 
 MIDDLEWARE = [
